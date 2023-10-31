70949890c7802c7923cec1b9da46e667431e574b
parvini82 <118878849+parvini82@users.noreply.github.com>
2023-10-31 10:27:23 +0330

    added package.json,tsconfig,viteconfig

---
+++ client/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  root: "src",
+  build: {
+    minify: "esbuild",
+    outDir: "",
+    rollupOptions: {
+        input: {
+            main: "src/index.html",
+            
+        },
+    },
+},
+})
