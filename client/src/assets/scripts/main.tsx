183519032c2e0f68c168d5981ebe585931769795
parvini82 <118878849+parvini82@users.noreply.github.com>
2023-11-02 12:42:11 +0330

    README.md added

--- client/src/assets/scripts/main.tsx
+++ client/src/assets/scripts/main.tsx
@@ -21,13 +21,9 @@ ReactDOM.createRoot(document.getElementById('posts')!).render(
      It has been a popular choice among developers for over two decades, with millions of Java applications 
      in use today.'
       date='Sep 24, 2023' readTime='3 min read' imageSrc='./assets/images/Java.png' imageAlt='image' readMoreLink='https://www.java.com/'></Post>
-      <Post title='Python' description='Java is a widely-used programming language for coding web applications.
-     It has been a popular choice among developers for over two decades, with millions of Java applications 
-     in use today.'
+      <Post title='Python' description='Python can be easy to pick up whether youre a first time programmer or youre experienced with other languages. The following pages are a useful first step to get on your way writing programs with Python!'
       date='Sep 24, 2023' readTime='3 min read' imageSrc='./assets/images/Java.png' imageAlt='image' readMoreLink='https://www.python.org/'></Post>
-      <Post title='Java' description='Java is a widely-used programming language for coding web applications.
-     It has been a popular choice among developers for over two decades, with millions of Java applications 
-     in use today.'
+      <Post title='JavaScript' description='JavaScript is a high-level, versatile, and widely-used programming language primarily used for adding interactivity and dynamic behavior to websites. It is one of the core technologies for building web applications and is supported by all modern web browsers, making it a fundamental part of web development.'
       date='Sep 24, 2023' readTime='3 min read' imageSrc='./assets/images/Java.png' imageAlt='image' readMoreLink='https://www.java.com/'></Post>
 
     
