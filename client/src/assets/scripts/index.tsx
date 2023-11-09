5e4bdf324b52a90947811ee95b8c3b0507c865d2
parvini82 <118878849+parvini82@users.noreply.github.com>
2023-11-09 12:18:45 +0330

    posts show on post page fixed !

--- client/src/assets/scripts/index.tsx
+++ client/src/assets/scripts/index.tsx
@@ -7,8 +7,7 @@ import Post from "./components/post";
 import PostPage from "./components/postpage";
 
 
-const currentUrl = window.location.href;
-console.log(currentUrl);
+
 function App() {
 	return (
 
@@ -25,9 +24,11 @@ function App() {
 }
 
 export default App;
+const currentUrl = window.location.href;
 
 ReactDOM.createRoot(document.getElementById('content')!).render(
 	<App></App>
+	
 
 )
 ReactDOM.createRoot(document.getElementById('footer')!).render(
@@ -35,38 +36,41 @@ ReactDOM.createRoot(document.getElementById('footer')!).render(
 
 
 )
-ReactDOM.createRoot(document.getElementById('posts')!).render(
-	<div>
-		<Post
-			title='Java'
-			description='Java is a widely-used programming language for coding web applications. It has been a popular choice among developers for over two decades, with millions of Java applications in use today.'
-			date='Sep 24, 2023'
-			readTime='3 min read'
-			imageSrc='./assets/images/Java.png'
-			imageAlt='image'
-			readMoreLink='https://www.java.com/'
-			postId="1"
-		/>
-		<Post
-			title='Python'
-			description='Python can be easy to pick up whether youre a first-time programmer or youre experienced with other languages. The following pages are a useful first step to get on your way writing programs with Python!'
-			date='Sep 24, 2023'
-			readTime='3 min read'
-			imageSrc='./assets/images/Java.png'
-			imageAlt='image'
-			readMoreLink='https://www.python.org/'
-			postId="2"
-		/>
-		<Post
-			title='JavaScript'
-			description='JavaScript is a high-level, versatile, and widely-used programming language primarily used for adding interactivity and dynamic behavior to websites. It is one of the core technologies for building web applications and is supported by all modern web browsers, making it a fundamental part of web development.'
-			date='Sep 24, 2023'
-			readTime='3 min read'
-			imageSrc='./assets/images/Java.png'
-			imageAlt='image'
-			readMoreLink='https://www.java.com/'
-			postId="3"
-		/>
-		<Outlet />
-	</div>
-);
+
+if (currentUrl.split("/")[3] != 'post') {
+	ReactDOM.createRoot(document.getElementById('posts')!).render(
+		<div>
+			<Post
+				title='Java'
+				description='Java is a widely-used programming language for coding web applications. It has been a popular choice among developers for over two decades, with millions of Java applications in use today.'
+				date='Sep 24, 2023'
+				readTime='3 min read'
+				imageSrc='./assets/images/Java.png'
+				imageAlt='image'
+				readMoreLink='https://www.java.com/'
+				postId="1"
+			/>
+			<Post
+				title='Python'
+				description='Python can be easy to pick up whether youre a first-time programmer or youre experienced with other languages. The following pages are a useful first step to get on your way writing programs with Python!'
+				date='Sep 24, 2023'
+				readTime='3 min read'
+				imageSrc='./assets/images/Java.png'
+				imageAlt='image'
+				readMoreLink='https://www.python.org/'
+				postId="2"
+			/>
+			<Post
+				title='JavaScript'
+				description='JavaScript is a high-level, versatile, and widely-used programming language primarily used for adding interactivity and dynamic behavior to websites. It is one of the core technologies for building web applications and is supported by all modern web browsers, making it a fundamental part of web development.'
+				date='Sep 24, 2023'
+				readTime='3 min read'
+				imageSrc='./assets/images/Java.png'
+				imageAlt='image'
+				readMoreLink='https://www.java.com/'
+				postId="3"
+			/>
+			<Outlet />
+		</div>
+	);
+}
