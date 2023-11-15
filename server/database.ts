d141fbcb050e060d3f2dc9e2660212d64d5de6e4
parvini82 <118878849+parvini82@users.noreply.github.com>
2023-11-15 15:57:23 +0330

    removed extra files

--- server/database.ts
+++
@@ -1,7 +0,0 @@
-import mysql from "mysql2/promise";
-async function main() {
-    const connection = await mysql.createConnection({ host: 'localhost', user: 'root', password: "", database: 'myblog' });
-    const [rows, fields] = await connection.execute('SELECT * FROM Posts');
-    console.log(rows);
-}
-main();
